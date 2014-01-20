bcf093aff18aab8b65182a9758dfa522d4839b36
Naotoshi Seo <sonots@gmail.com>
2014-01-20 16:40:33 +0900

    cleanup tag_prefix and tag_suffix

--- lib/fluent/plugin/out_record_reformer.rb
+++ lib/fluent/plugin/out_record_reformer.rb
@@ -59,19 +59,21 @@ module Fluent
 
     def emit(tag, es, chain)
       tag_parts = tag.split('.')
+      tag_prefix = tag_prefix(tag_parts)
+      tag_suffix = tag_suffix(tag_parts)
       es.each { |time, record|
-        new_tag, new_record = reform(@output_tag, record, tag, tag_parts, @time_proc.call(time))
+        new_tag, new_record = reform(@output_tag, record, tag, tag_parts, tag_prefix, tag_suffix, @time_proc.call(time))
         Engine.emit(new_tag, time, new_record)
       }
       chain.next
     rescue => e
-      $log.warn "record_reformer: #{e.class} #{e.message} #{e.backtrace.join(', ')}"
+      $log.warn "record_reformer: #{e.class} #{e.message} #{e.backtrace.first}"
     end
 
     private
 
-    def reform(output_tag, record, tag, tag_parts, time)
-      @placeholder_expander.prepare_placeholders(record, tag, tag_parts, @hostname, time)
+    def reform(output_tag, record, tag, tag_parts, tag_prefix, tag_suffix, time)
+      @placeholder_expander.prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, @hostname, time)
       new_tag = @placeholder_expander.expand(output_tag)
 
       new_record = @renew_record ? {} : record.dup
@@ -81,11 +83,30 @@ module Fluent
       [new_tag, new_record]
     end
 
+    def tag_prefix(tag_parts)
+      return [] if tag_parts.empty?
+      tag_prefix = [tag_parts.first]
+      1.upto(tag_parts.size-1).each do |i|
+        tag_prefix[i] = "#{tag_prefix[i-1]}.#{tag_parts[i]}"
+      end
+      tag_prefix
+    end
+
+    def tag_suffix(tag_parts)
+      return [] if tag_parts.empty?
+      rev_tag_parts = tag_parts.reverse
+      rev_tag_suffix = [rev_tag_parts.first]
+      1.upto(tag_parts.size-1).each do |i|
+        rev_tag_suffix[i] = "#{rev_tag_parts[i]}.#{rev_tag_suffix[i-1]}"
+      end
+      rev_tag_suffix.reverse
+    end
+
     class PlaceholderExpander
       # referenced https://github.com/fluent/fluent-plugin-rewrite-tag-filter, thanks!
       attr_reader :placeholders
 
-      def prepare_placeholders(record, tag, tag_parts, hostname, time)
+      def prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, hostname, time)
         placeholders = {
           '${time}' => time,
           '${tag}' => tag,
@@ -93,26 +114,24 @@ module Fluent
         }
 
         size = tag_parts.size
-        tag_prefix = nil
-        tag_suffix = nil
 
         tag_parts.each_with_index { |t, idx|
           placeholders.store("${tag_parts[#{idx}]}", t)
           placeholders.store("${tag_parts[#{idx-size}]}", t) # support tag_parts[-1]
-
-          tag_prefix = tag_prefix ? (tag_prefix + '.' + t) : t
-          placeholders.store("${tag_prefix[#{idx}]}", tag_prefix)
-          placeholders.store("${tag_prefix[#{idx-size}]}", tag_prefix) # support tag_prefix[-1]
 
           # tags is just for old version compatibility
           placeholders.store("${tags[#{idx}]}", t)
           placeholders.store("${tags[#{idx-size}]}", t) # support tags[-1]
         }
 
-        tag_parts.reverse.each_with_index { |t, idx| # idx is reversed in this loop
-          tag_suffix = tag_suffix ? (t + '.' + tag_suffix) : t
-          placeholders.store("${tag_suffix[#{size-idx-1}]}", tag_suffix)
-          placeholders.store("${tag_suffix[#{-idx-1}]}", tag_suffix) # support tag_suffix[-1]
+        tag_prefix.each_with_index { |t, idx|
+          placeholders.store("${tag_prefix[#{idx}]}", t)
+          placeholders.store("${tag_prefix[#{idx-size}]}", t) # support tag_prefix[-1]
+        }
+
+        tag_suffix.each_with_index { |t, idx|
+          placeholders.store("${tag_suffix[#{idx}]}", t)
+          placeholders.store("${tag_suffix[#{idx-size}]}", t) # support tag_suffix[-1]
         }
 
         record.each { |k, v|
@@ -124,7 +143,7 @@ module Fluent
 
       def expand(str)
         str.gsub(/(\${[a-z_]+(\[-?[0-9]+\])?}|__[A-Z_]+__)/) {
-          $log.warn "record_reformer: unknown placeholder `#{$1}` found in a tag `#{tag}`" unless @placeholders.include?($1)
+          $log.warn "record_reformer: unknown placeholder `#{$1}` found" unless @placeholders.include?($1)
           @placeholders[$1]
         }
       end
@@ -138,29 +157,16 @@ module Fluent
       # @param [Hash]   record      the record, one of information
       # @param [String] tag         the tag
       # @param [Array]  tag_parts   the tag parts (tag splitted by .)
+      # @param [Array]  tag_prefix  the tag prefix parts
+      # @param [Array]  tag_suffix  the tag suffix parts
       # @param [String] hostname    the hostname
       # @param [Time]   time        the time
-      def prepare_placeholders(record, tag, tag_parts, hostname, time)
-        tag_prefix = []
-        tag_suffix = []
-        tmp_tag_prefix = nil
-        tmp_tag_suffix = nil
-
-        tag_parts.each { |t|
-          tmp_tag_prefix = tmp_tag_prefix ? (tmp_tag_prefix + '.' + t) : t
-          tag_prefix << tmp_tag_prefix
-        }
-
-        tag_parts.reverse.each { |t|
-          tmp_tag_suffix = tmp_tag_suffix ? (t + '.' + tmp_tag_suffix) : t
-          tag_suffix << tmp_tag_suffix
-        }
-
+      def prepare_placeholders(record, tag, tag_parts, tag_prefix, tag_suffix, hostname, time)
         struct = UndefOpenStruct.new(record)
         struct.tag  = tag
         struct.tags = struct.tag_parts = tag_parts # tags is for old version compatibility
         struct.tag_prefix = tag_prefix
-        struct.tag_suffix = tag_suffix.reverse
+        struct.tag_suffix = tag_suffix
         struct.time = time
         struct.hostname = hostname
         @placeholders = struct
